e5e66de07d465422b98edb7c7a972d6974da229c
airan.nascimento <airan.nascimento@26ADM23193.cooperativalanguiru.local>
2020-10-08 10:11:07 -0300

    - Adição de biblioteca e classe padrão para comunicação Serial.

---
+++ src/main/java/com/agf/sinalescolar/utils/SerialCommunaction.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agf.sinalescolar.utils;
+
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
+/**
+ *
+ * @author airan.nascimento
+ */
+public class SerialCommunaction {
+    private static final int BAUD_RATE = 9600;
+    private static final int DATA_BITS = 8;
+    private static final int STOP_BITS = 1;
+    private static final int PARITY    = 0;
+    private static final String SERIAL_PORT = "COM4";
+    
+    
+    /* Verificar como fazer, executar uma Thread para ficar pegando a hora do sistema
+    e ver se bate com alguma hora na lista de ocorrências. Quando bater, enviar
+    mensagem para tocar, senão Thread.sleep e reinicia de tanto em tanto tempo. */
+    public static void enviar(String dados) {
+        SerialPort serialPort = new SerialPort(SERIAL_PORT);
+        
+        try {     	
+            System.out.println("Porta aberta: " + serialPort.openPort());
+            System.out.println("Parâmetros configurados: " + serialPort.setParams(BAUD_RATE, DATA_BITS, STOP_BITS, PARITY));
+            System.out.println("Mensagem enviada: " + serialPort.writeBytes(dados.getBytes()));
+            System.out.println("Porta fechada: " + serialPort.closePort());
+        }
+        catch (SerialPortException ex){
+            System.out.println(ex);
+        }
+    }
+}
